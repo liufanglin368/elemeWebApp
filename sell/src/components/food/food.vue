e67203a74add91456bef9479c2d84edaf887e7be
liufanglin <liufanglin368@163.com>
2018-07-09 11:44:54 +0800

    split

--- sell/src/components/food/food.vue
+++ sell/src/components/food/food.vue
@@ -17,15 +17,20 @@
 					<div class="price">
 						<span class="now">¥{{food.price}}</span><span class="old" v-show='food.oldPrice'>¥{{food.oldPrice}}</span>
 					</div>
-				</div>
-				<div class="cartcontrol-wrapper">
-					<cartcontrol  @add="addFood" :food="food"></cartcontrol>
-				</div>
-				<transition name="fade">
-					<div @click.stop.prevent="addFirst" class='buy' v-show="!food.count || food.count ===0">
-					加入购物车
+					<div class="cartcontrol-wrapper">
+						<cartcontrol  @add="addFood" :food="food"></cartcontrol>
 					</div>
-				</transition>
+					<transition name="fade">
+						<div @click.stop.prevent="addFirst" class='buy' v-show="!food.count || food.count ===0">
+					加入购物车
+						</div>
+					</transition>
+				</div>
+				<split  v-show="food.info"></split>	
+				<div class="info" v-show="food.info">
+					<h1 class="title">商品信息</h1>
+					<p class="text">{{food.info}}</p>		
+				</div>		
 			</div>
 		</div>
 	</transition>
@@ -35,6 +40,7 @@
  	import BScroll from 'better-scroll'; 
  	import Vue from 'vue'; 
  	import cartcontrol from 'components/cartcontrol/cartcontrol';
+ 	import split from 'components/split/split';
 
   export default {
   	props: {
@@ -75,7 +81,8 @@
       	}
   	},
   	components: {
-  		cartcontrol
+  		cartcontrol,
+  		split
   	}
   };
 
@@ -121,6 +128,7 @@
 	    	}
 	    }
 	    .content{
+	    	position: relative;
 	    	padding: 18px;
 	    	.title{
 	    		line-height: 14px;
@@ -156,32 +164,48 @@
 					color: rgb(147,153,159);
 				}
 			}
+			.cartcontrol-wrapper{
+		    	position: absolute;
+		    	right: 12px;
+		    	bottom: 12px;
+		    }
+		    .buy{
+		    	position: absolute;
+		    	right: 18px;
+		    	bottom: 18px;
+		    	z-index: 10;
+		    	line-height: 24px;
+		    	padding: 0 12px;
+		    	box-sizing: border-box;
+		    	font-size: 10px;
+		    	border-radius: 12px;
+		    	color: #fff;
+		    	background: rgb(0, 160, 220);
+		    	opacity: 1;
+		    	&.fade-enter-active, &.fade-leave-active{
+		          transition: all 0.2s;
+		    	}
+		        &.fade-enter, &.fade-leave-active{
+		          opacity: 0;
+		          z-index: -1;
+		        }
+		    }
 	    }
-	    .cartcontrol-wrapper{
-	    	position: absolute;
-	    	right: 12px;
-	    	bottom: 12px;
-	    }
-	    .buy{
-	    	position: absolute;
-	    	right: 18px;
-	    	bottom: 18px;
-	    	z-index: 10;
-	    	line-height: 24px;
-	    	padding: 0 12px;
-	    	box-sizing: border-box;
-	    	font-size: 10px;
-	    	border-radius: 12px;
-	    	color: #fff;
-	    	background: rgb(0, 160, 220);
-	    	opacity: 1;
-	    	&.fade-enter-active, &.fade-leave-active{
-	          transition: all 0.2s;
+	    .info{
+	    	padding: 18px;
+	    	.title{
+	    		line-height: 14px;
+	    		margin-bottom: 6px;
+	    		font-size: 14px;
+	    		color: rgb(7, 17, 27);
 	    	}
-	        &.fade-enter, &.fade-leave-active{
-	          opacity: 0;
-	          z-index: -1;
-	        }
+	    	.text{
+	    		line-height: 24px;
+	    		padding: 0 8px;
+	    		font-size: 12px;
+	    		color: rgb(77, 85, 93);
+	    	}
 	    }
+	   
 	}
 </style>
